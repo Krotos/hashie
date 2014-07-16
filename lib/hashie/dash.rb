cf806d8c72b087f02c16c4023ba4846f39366c07
Krotos <krotos2112@gmail.com>
2014-07-16 16:01:27 +0400

    Code refactoring

--- lib/hashie/dash.rb
+++ lib/hashie/dash.rb
@@ -13,10 +13,11 @@ module Hashie
       @data = {}
       self.class.local_data.each do |key, value|
         @data[key] = hash[key] || value[:default]
-        raise ArgumentError if @data[key].nil? && value[:required]
+        assert_raise_argument_error(key, @data[key])
+
         define_singleton_method(:"#{key}=") do |defined_value|
-          raise ArgumentError if defined_value.nil? && self.class.local_data[key][:required]
-          @data[key] = defined_value
+          assert_raise_argument_error(key, defined_value)
+          @data[key] = defined_value || value[:default]
         end
 
         define_singleton_method(key) do
@@ -31,8 +32,17 @@ module Hashie
     end
 
     def []=(key, value)
-      raise ArgumentError if value.nil? && self.class.local_data[key][:required]
-      @data[key] = value
+      assert_raise_argument_error(key, value)
+      @data[key] = value || self.class.local_data[key][:default]
+    end
+
+    def has_default?(key)
+      self.class.local_data[key].has_key?(:default)
+    end
+
+    def assert_raise_argument_error(key, value)
+      raise ArgumentError, "ArgumentError: The property '#{key}' is required for this Dash." \
+        if value.nil? && self.class.local_data[key][:required] && !has_default?(key)
     end
   end
 end
